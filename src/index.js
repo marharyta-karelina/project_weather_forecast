0e8517b466b66aa7eb926945c82a26f486b4c36a
marharyta-karelina <rita.karelina94@gmail.com>
2022-09-22 18:46:12 +0300

    Change sequence days for forecast

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ currentDayTime.innerHTML = formatDate(currentTime);
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
   return days[day];
 }
 
